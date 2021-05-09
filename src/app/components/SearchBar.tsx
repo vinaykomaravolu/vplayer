1a06db8d9818aec7ab16ac133d0e8d1feecce95d
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-05-09 03:53:09 -0400

    Adds search bar

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+const SearchIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-8 w-8"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const CloseIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+function SearchBar(props: { setState: any; state: string; handleSearch: any }) {
+  const { setState, state, handleSearch } = props;
+  const node = useRef<any>();
+  const [searchIsOpen, setSearchIsOpen] = useState<boolean>(true);
+
+  const handleClick = (event: any) => {
+    if (node.current.contains(event.target)) {
+      return;
+    }
+    if (node.current.value !== '') {
+      return;
+    }
+    setState('');
+    setSearchIsOpen(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  const onEnter = (event: any) => {
+    if (event.key === 'Enter') {
+      handleSearch(event);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <input
+        ref={node}
+        type="text"
+        className={`h-10  ${
+          searchIsOpen ? 'opacity-0 w-0' : 'opacity-100 w-64 '
+        } pl-12 pr-9  rounded-full z-0  transition-width transition-slowest ease duration-300 ease-in-out  focus:shadow focus:outline-none bg-primary2 text-white`}
+        placeholder="Search Music"
+        disabled={searchIsOpen}
+        onChange={(event) => {
+          setState(event.target.value);
+        }}
+        value={state}
+        onKeyPress={onEnter}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          setSearchIsOpen(!searchIsOpen);
+        }}
+        className="focus:outline-none absolute top-1 left-2 text-secondary hover:text-secondary2"
+      >
+        {SearchIcon}
+      </button>
+      {state !== '' ? (
+        <button
+          type="button"
+          onClick={(event) => {
+            setState('');
+            handleSearch(event);
+          }}
+          className="focus:outline-none absolute top-2.5 right-2 text-secondary hover:text-secondary2"
+        >
+          {CloseIcon}
+        </button>
+      ) : null}
+    </div>
+  );
+}
+
+export default SearchBar;
--- src/app/pages/AllMusic.tsx
+++ src/app/pages/AllMusic.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { motion } from 'framer-motion';
+import SearchBar from '../components/SearchBar';
 
 const PlayIcon = (
   <svg
@@ -16,72 +17,15 @@ const PlayIcon = (
   </svg>
 );
 
-const SearchIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-8 w-8"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-  >
-    <path
-      fillRule="evenodd"
-      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
 const pageVariants = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
 };
 
-function SearchBar() {
-  const node = useRef<any>();
-  const [searchIsOpen, setSearchIsOpen] = useState<boolean>(true);
-
-  const handleClick = (event: any) => {
-    if (node.current.contains(event.target)) {
-      // inside click
-      return;
-    }
-    // outside click
-    setSearchIsOpen(true);
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClick);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClick);
-    };
-  }, []);
-  return (
-    <div ref={node} className="relative">
-      <input
-        type="text"
-        className={`h-10 ${searchIsOpen ? 'w-0' : 'w-64'} ${
-          searchIsOpen ? 'opacity-0' : 'opacity-100'
-        }  pl-12 pr-4 rounded-full z-0  transition-width transition-slowest ease duration-300 ease-in-out  focus:shadow focus:outline-none bg-primary2 text-white`}
-        placeholder="Search Music"
-        disabled={searchIsOpen}
-      />
-      <button
-        type="button"
-        onClick={() => {
-          setSearchIsOpen(!searchIsOpen);
-        }}
-        className="focus:outline-none absolute top-1 left-2 text-secondary hover:text-secondary2"
-      >
-        {SearchIcon}
-      </button>
-    </div>
-  );
-}
-
 function AllMusic() {
   const [songs, setSongs] = useState<any>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
     const songsList = [];
@@ -90,7 +34,7 @@ function AllMusic() {
         <motion.div
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className="bg-gradient-to-r m-1 p-6 from-yellow-400 via-red-500 to-pink-500 rounded-full "
+          className="bg-gradient-to-r m-1 p-6 from-secondary via-red-500 to-red-500  rounded-full opacity-30"
         />
       );
     }
@@ -119,7 +63,13 @@ function AllMusic() {
             </motion.button>
           </div>
           <div id="search">
-            <SearchBar />
+            <SearchBar
+              state={searchTerm}
+              setState={setSearchTerm}
+              handleSearch={(event: any) => {
+                console.log(`Searching ${event.target.value}`);
+              }}
+            />
           </div>
         </div>
         <div className="flex flex-col h-full overflow-y-auto">{songs}</div>
