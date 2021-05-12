a70763ac83c6fb3cb23087577126e19dcacfd268
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-05-11 22:16:43 -0400

    Created playlist collectionr routing page

--- src/app/components/PlayListCard.tsx
+++ src/app/components/PlayListCard.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import { Playlist } from '../../objects/Object';
 
 function PlayListCard({ playlist }: { playlist: Playlist }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <motion.button
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      className="shadow-md focus:outline-none bg-gradient-to-t from-primary via-primary2 to-primary3 w-44 h-48 rounded-2xl hover:to-secondary2 hover:from-secondary2 hover:via-secondary2"
-    >
-      <div id="content" className="p-2 h-full w-full">
-        <p className="text-2xl overflow-ellipsis overflow-hidden text-white">
-          {playlist.name}
-        </p>
-      </div>
-    </motion.button>
+    <Link to={`/playlists/${playlist.name}`}>
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className="shadow-md focus:outline-none bg-gradient-to-t from-primary via-primary2 to-primary3 w-44 h-48 rounded-2xl hover:to-secondary2 hover:from-secondary2 hover:via-secondary2"
+      >
+        <div id="content" className="p-2 h-full w-full">
+          <p className="text-2xl overflow-ellipsis overflow-hidden text-white">
+            {playlist.name}
+          </p>
+        </div>
+      </motion.button>
+    </Link>
   );
 }
 
--- src/app/pages/Albums.tsx
+++ src/app/pages/Albums.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+const pageVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
 function Albums() {
   return (
     <motion.div
+      variants={pageVariants}
       initial="initial"
       animate="animate"
       exit="exit"
       className="section-page"
+      id="albums"
     >
       Albums
     </motion.div>
