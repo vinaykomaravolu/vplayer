a239881df07625fc3c3065a1c3419516813d72ae
shadowfox137 <icefox137@outlook.com>
2021-05-12 15:53:34 -0400

    adds small changes to the music player

--- src/app/components/MusicPlayer.tsx
+++ src/app/components/MusicPlayer.tsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import '!style-loader!css-loader!rc-slider/assets/index.css';
 import React, { useState } from 'react';
-import Slider, { Range } from 'rc-slider';
+import Slider from 'rc-slider';
 import { IoMdSkipBackward, IoMdSkipForward } from 'react-icons/io';
+import { AiFillSound } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
 const PlayIcon = (
@@ -57,30 +58,58 @@ const IconButton = (props: { icon: any; handleClick: () => void }) => {
 
 function MusicPlayer() {
   const [isTimelineHover, setIsTimelineHover] = useState<boolean>(false);
+  const [isVolumelineHover, setVolumelineHover] = useState<boolean>(false);
   let currentTime = 0;
+  let maxTime = 0;
 
-  function updateCurrentTime(time: number) {
-    currentTime = time;
+  function updateCurrentTime(current: number) {
+    currentTime = current;
+  }
+
+  function updateMaxTime(max: number) {
+    maxTime = max;
+  }
+
+  function secondsToMin(d: number) {
+    // eslint-disable-next-line no-param-reassign
+    d = Number(d);
+    return Math.floor((d % 3600) / 60);
+  }
+
+  function secondsToSec(d: number) {
+    // eslint-disable-next-line no-param-reassign
+    d = Number(d);
+    return Math.floor((d % 3600) % 60);
   }
 
   return (
     <div className="bg-primary3 flex flex-row justify-center h-full w-full">
-      <div className="flex flex-col w-full justify-center">
+      <div className="flex flex-col justify-center content-center w-1/5 pr-2 pl-2">
+        <div className="flex flex-row h-3/4 w-full content-center justify-center bg-yellow-400 overflow-hidden">
+          <div className="h-full w-1/3 bg-white">test</div>
+          <div className="flex flex-col w-2/3 justify-end content-end overflow-hidden bg-black">
+            Info
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-3/5 justify-center pr-2 pl-2 bg-yellow-100">
         <div className="flex flex-row justify-center content-center">
           <div className="flex flex-nowrap content-center">
             <IconButton icon={<IoMdSkipBackward />} handleClick={() => {}} />
           </div>
           <div className="flex flex-nowrap content-center">
             <IconButton icon={PlayIcon} handleClick={() => {}} />
-          </div>{' '}
+          </div>
           <div className="flex flex-nowrap content-center">
             <IconButton icon={<IoMdSkipForward />} handleClick={() => {}} />
           </div>
         </div>
         <div className="flex flex-row justify-center w-full ">
-          <div className="text-sm pr-3 pl-3 text-white "> {currentTime} </div>
+          <div className="text-sm pr-3 pl-3 text-white ">
+            {secondsToMin(currentTime)}:{secondsToSec(currentTime)}
+          </div>
           <div
-            className="w-1/2 p-0.5"
+            className="w-3/4 p-0.5"
             onMouseEnter={() => setIsTimelineHover(true)}
             onMouseLeave={() => setIsTimelineHover(false)}
           >
@@ -115,7 +144,49 @@ function MusicPlayer() {
             />
           </div>
 
-          <div className="text-sm pr-4 pl-4 text-white"> Timer2 </div>
+          <div className="text-sm pr-4 pl-4 text-white">
+            {secondsToMin(maxTime)}:{secondsToSec(maxTime)}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-end content-start w-1/5 pr-2 pl-2 bg-white">
+        <div className="flex flex-nowrap content-center">
+          <IconButton icon={<AiFillSound />} handleClick={() => {}} />
+        </div>
+        <div
+          className="w-1/2 p-0.5"
+          onMouseEnter={() => setVolumelineHover(true)}
+          onMouseLeave={() => setVolumelineHover(false)}
+        >
+          <Slider
+            style={{ width: '100%' }}
+            reverse={false}
+            min={0 /** Change this */}
+            max={100 /** Change this */}
+            step={1 /** Change this */}
+            trackStyle={{ backgroundColor: '#FFD700', height: 6 }}
+            handleStyle={
+              isVolumelineHover
+                ? {
+                    borderColor: '#FFD700',
+                    height: 16,
+                    width: 16,
+                    backgroundColor: '#151A21',
+                  }
+                : {
+                    borderColor: '#FFD700',
+                    height: 6,
+                    width: 0,
+                    marginTop: 0,
+                    backgroundColor: '#FFD700',
+                  }
+            }
+            railStyle={{
+              backgroundColor: '#F2AA4C',
+              height: 6,
+              opacity: 0.5,
+            }}
+          />
         </div>
       </div>
     </div>
